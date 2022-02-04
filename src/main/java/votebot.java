c6ce3dcb2453daa22dfcb2a655ed201c54ed0783
ItsLukass <itslukass123@gmail.com>
2022-02-04 18:49:08 +0200

    Cleaned up the code so its actually readable

--- src/main/java/votebot.java
+++ src/main/java/votebot.java
@@ -1,130 +1,107 @@
 import com.github.javafaker.Faker;
+import com.sun.jna.platform.win32.Netapi32Util;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Screen;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Scanner;
-
-
-/*
-                String PROXY = "88.198.24.108:3128";
-
-        org.openqa.selenium.Proxy proxy = new org.openqa.selenium.Proxy();
-        proxy.setHttpProxy(PROXY)
-                .setFtpProxy(PROXY)
-                .setSslProxy(PROXY);
-        DesiredCapabilities cap = new DesiredCapabilities();
-        cap.setCapability(CapabilityType.PROXY, proxy);
-
-        ChromeDriver browser = new ChromeDriver(cap);
-         */
-//Proxy proxy = new Proxy();
-//proxy.setHttpProxy("78.47.195.69:8888");
 
 
 public class votebot {
 
     public static final String USERNAME_ID = "player";
+    public static final String Link = "https://m-craft.lt/vote/mc-meemso-eu-factions-mcmmo/e25b01ec8f4032b14b7b01b3a6c21a2e";
+    static ChromeDriver browser;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws FindFailed, InterruptedException {
 
         // setting everything up
-
-        // opening the proxies.txt file
-        //FileInputStream fstream = new FileInputStream("proxys.txt");
-        //BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-
-        // setting everything for for chrome
         System.setProperty("webdriver.chrome.driver", "src/webdriver/chromedriver97.exe");
         System.out.println("\n" + "Kiek votes?");
         Scanner in = new Scanner(System.in);
         int amount = in.nextInt();
-        int proxynr = 5;
         while (amount > 0) {
-            String proxylist = Files.readAllLines(Paths.get("proxys.txt")).get(proxynr);
 
-            ChromeOptions chromeOptions = new ChromeOptions();
-
-            Proxy proxy = new Proxy();
-            proxy.setAutodetect(false);
-            proxy.setHttpProxy(proxylist);
-            proxy.setSslProxy(proxylist);
-            proxy.setNoProxy("no_proxy-var");
-
-            chromeOptions.setCapability("proxy", proxy);
-
-            //creating the browser window and opening m-craft page
-            ChromeDriver browser = new ChromeDriver(chromeOptions);
-            browser.get("https://m-craft.lt/vote/mc-meemso-eu-factions-mcmmo/c46fcd272434e15974679b53cde90381");
-
+            // Sets up chrome and opens it
+            OpenChrome();
 
             // waiting for page to load for 3 sec
-            try {
-                // laukiame 3 sec
-                Thread.sleep(3000);
-            } catch (InterruptedException ex) {
-                Thread.currentThread().interrupt();
-            }
-
-            // clicking balsuoti button to open the main voting page
-            // WebElement click2 = browser.findElement(By.xpath("//button[contains(.,'Balsuoti')]"));
-            //JavascriptExecutor executor2 = (JavascriptExecutor) browser;
-            //executor2.executeScript("arguments[0].click();", click2);
+            Wait(3000);
 
             // username field
-
-            Faker faker = new Faker();
-            String firstName = faker.name().firstName();
-            String lastName = faker.name().lastName();
-
-            WebElement username = browser.findElement(By.id(USERNAME_ID));
-            username.sendKeys(firstName + "_" + lastName);
+            UsernameInput();
 
             // recaptcha
+            CaptchaClick();
 
-            Actions builder = new Actions(browser);
-            builder.sendKeys(Keys.TAB).perform();
-            builder.sendKeys(Keys.SPACE).perform();
+            // Wait 2 seconds to load
+            Wait(2000);
 
-            // vote button confirm
+            // in case of recaptcha asking you to actually solve the challenge
+            //clicking buster extension button
+            CaptchaSolve();
 
-            try {
-                // laukiame 2 sec
-                Thread.sleep(2000);
-            } catch (InterruptedException ex) {
-                Thread.currentThread().interrupt();
-            }
+            // Waiting for the captcha to be solved (or not xD)
+            Wait(12000);
+            // Clicking Vote button
+            ClickVote();
 
+            // Waiting for everything to finish
+            Wait(5000);
 
-            WebElement click = browser.findElement(By.xpath("//button[contains(.,'Balsuoti')]"));
-            JavascriptExecutor executor = (JavascriptExecutor) browser;
-            executor.executeScript("arguments[0].click();", click);
-
-
-            // palaukiame kol rankiniu budu ispresime captcha
-            try {
-                // laukiame 10 sec
-                Thread.sleep(10000);
-            } catch (InterruptedException ex) {
-                Thread.currentThread().interrupt();
-            }
             browser.close();
-
-            amount = amount - 1;
-            proxynr++;
+            amount--;
 
         }
-
-        }
-
 
     }
+
+    public static void CaptchaSolve() {
+        System.out.println("clicking captcha solve button");
+
+    }
+
+    public static void ClickVote() {
+        System.out.println("Clicking the vote button");
+        WebElement click = browser.findElement(By.xpath("//button[contains(.,'Balsuoti')]"));
+        JavascriptExecutor executor = browser;
+        executor.executeScript("arguments[0].click();", click);
+    }
+
+    public static void Wait(int time) throws InterruptedException {
+        Thread.sleep(time);
+    }
+
+    public static void OpenChrome() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addExtensions(new File("solver.crx"));
+
+        //creating the browser window and opening m-craft page
+        browser = new ChromeDriver(chromeOptions);
+        browser.get(Link);
+    }
+
+    public static void UsernameInput() {
+
+        Faker faker = new Faker();
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+
+        WebElement username = browser.findElement(By.id(USERNAME_ID));
+        username.sendKeys(firstName + "_" + lastName);
+    }
+
+    public static void CaptchaClick() throws InterruptedException {
+        Actions builder = new Actions(browser);
+        builder.sendKeys(Keys.TAB).perform();
+        builder.sendKeys(Keys.SPACE).perform();
+        Wait(1000);
+    }
+
+
+}
